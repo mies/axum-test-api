62c5ccd7f7514988bdb5afb43a41c34534f0066b
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2023-01-30 20:27:28 +0000

    Update Cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use axum::{
+    body::Body,
+    extract::{FromRef, FromRequestParts, State},
+    http::{Request, StatusCode},
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+
+use sqlx::postgres::{PgPool, PgPoolOptions};
+
+use autometrics::{autometrics, encode_global_metrics, global_metrics_exporter};
+
+use serde::{Deserialize, Serialize};
+use std::{net::SocketAddr, time::Duration};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
